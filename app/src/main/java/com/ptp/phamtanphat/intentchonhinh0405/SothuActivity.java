f588563629a3cd87d88676ee43c47fd6c8c7675d
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-20 17:52:17 +0700

    "Update view sothu"

--- app/src/main/java/com/ptp/phamtanphat/intentchonhinh0405/SothuActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/intentchonhinh0405/SothuActivity.java
@@ -3,13 +3,33 @@ package com.ptp.phamtanphat.intentchonhinh0405;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class SothuActivity extends AppCompatActivity {
 
+    TableLayout tableLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sothu);
-        Log.d("BBB",MainActivity.manghinh.size() + "");
+
+        tableLayout = findViewById(R.id.tablelayout);
+
+        int sohang = 6;
+        int socot = 3;
+
+        for (int i = 0 ; i < sohang ; i++){
+            TableRow tableRow = new TableRow(this);
+            for (int y = 0 ; y < socot ; y++){
+                ImageView imageView = new ImageView(this);
+                imageView.setImageResource(R.drawable.bo);
+                tableRow.addView(imageView);
+            }
+            tableLayout.addView(tableRow);
+        }
+
+
     }
 }
