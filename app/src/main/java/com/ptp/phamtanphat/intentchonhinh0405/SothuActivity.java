da1fcae0c7bed94342e37a605b6a3cb6d0edce89
thuviengithub0208 <khoaphp@yahoo.com>
2018-06-20 18:33:05 +0700

    "Cach lay gia tri man hinh"

--- app/src/main/java/com/ptp/phamtanphat/intentchonhinh0405/SothuActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/intentchonhinh0405/SothuActivity.java
@@ -2,6 +2,7 @@ package com.ptp.phamtanphat.intentchonhinh0405;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TableLayout;
@@ -10,6 +11,7 @@ import android.widget.TableRow;
 public class SothuActivity extends AppCompatActivity {
 
     TableLayout tableLayout;
+    int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +21,27 @@ public class SothuActivity extends AppCompatActivity {
 
         int sohang = 6;
         int socot = 3;
-
+//Cach lay chieu ngang va cao thiet bi
+//        DisplayMetrics displayMetrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+//        int height = displayMetrics.heightPixels;
+//        int width = displayMetrics.widthPixels;
+//
+//        Log.d("BBB",height + "");
+//        Log.d("BBB",width + "");
         for (int i = 0 ; i < sohang ; i++){
             TableRow tableRow = new TableRow(this);
-            for (int y = 0 ; y < socot ; y++){
+            for (int y = 0; y <socot ; y++){
+
                 ImageView imageView = new ImageView(this);
-                imageView.setImageResource(R.drawable.bo);
+                index = socot * (i ) + y ;
+                int idhinh = getResources().getIdentifier(MainActivity.manghinh.get(index),"drawable",getPackageName());
+                TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(250,250);
+                imageView.setLayoutParams(layoutParams);
+                imageView.setImageResource(idhinh);
                 tableRow.addView(imageView);
             }
+
             tableLayout.addView(tableRow);
         }
 
